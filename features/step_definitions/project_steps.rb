d0e66b38b996ca4d98499052c4940b6f86d38293
Zane5 <fat.fox@gmail.com>
2013-04-26 19:06:28 +0800

    Creation of projects feature complete

---
+++ features/step_definitions/project_steps.rb
@@ -0,0 +1,49 @@
+Given /^I am on the homepage$/ do
+  visit "/"
+end
+
+When /^I follow "([^"]*)"$/ do |link|
+  click_link link
+end
+
+When /^I fill in "([^"]*)" with "([^"]*)"$/ do |field, value|
+  fill_in field, :with => value
+end
+
+When /^I press "([^"]*)"$/ do |button|
+  click_button(button)
+end
+
+Then /^I should see "([^"]*)"$/ do |content|
+  page.should have_content(content)
+end
+
+Then /^I should be on the project page for "([^"]*)"$/ do |name|
+  project_path(Project.find_by_name!(name))
+end
+
+=begin
+
+Given /^(?:|I)am on (.+)$/ do |page_name|
+#  pending # express the regexp above with the code you wish you had
+  visit path_to(page_name)
+end
+
+When(/^I follow "(.*?)"$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I fill in "(.*?)" with "(.*?)"$/) do |arg1, arg2|
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I press "(.*?)"$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should see "(.*?)"$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+=end
+
