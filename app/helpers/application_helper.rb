024a8471c8013c9acca47f787c36a3649f4e550d
Zane5 <fat.fox@gmail.com>
2013-04-28 14:09:13 +0800

    Add validation to ensure names are specified when creating projects

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def title(*parts)
+    unless parts.empty?
+      content_for :title do
+        (parts << "Ticketee32").join(" - ") unless parts.empty?
+      end
+    end
+  end
 end
